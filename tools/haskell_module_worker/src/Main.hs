ae7d6a6a2aeb97e534eb10eca2153780ba8d1056
Facundo Domínguez <facundo.dominguez@tweag.io>
2022-03-30 10:25:51 -0300

    Implement a check to terminate the worker if it uses too much memory

--- tools/haskell_module_worker/src/Main.hs
+++ tools/haskell_module_worker/src/Main.hs
@@ -3,18 +3,23 @@ module Main where
 
 import Compile (runSession, compile)
 import Control.Exception (try)
+import Control.Monad (unless, when)
+import Control.Monad.IO.Class (liftIO)
+import Data.Word (Word64)
 import GHC.IO.Handle (hDuplicate, hDuplicateTo)
+import GHC.Stats (getRTSStats, getRTSStatsEnabled, max_live_bytes)
+import Options (parseArgs)
 import System.Environment (getArgs)
 import System.IO (Handle, hPrint, stderr, stdout)
 import System.IO.Error (alreadyInUseErrorType, ioeGetErrorType)
 
 main :: IO ()
 main = do
+    (args, memoryAllowance) <- getArgs >>= parseArgs
     stdout_dup <- redirectStdoutToStderr
-    args <- getArgs
     st <- runSession $ do
       st <- compile args 0
-      liftIO $ terminateIfUsingTooMuchMemory
+      liftIO $ terminateIfUsingTooMuchMemory memoryAllowance
       return st
     hPrint stdout_dup st
 
@@ -35,3 +40,15 @@ redirectStdoutToStderr = do
         redirectRetrying (i - 1)
       _ ->
         return ()
+
+-- | Terminates the worker if it exceeds the memory allowance
+terminateIfUsingTooMuchMemory :: Word64 -> IO ()
+terminateIfUsingTooMuchMemory memoryAllowance = do
+    statsEnabled <- getRTSStatsEnabled
+    unless statsEnabled $
+      error "terminateIfUsingTooMuchMemory: worker built without -with-rtsopts=-T"
+    stats <- getRTSStats
+    when (max_live_bytes stats > memoryAllowance * 1024 * 1024) $
+      error $
+        "terminateIfUsingTooMuchMemory: worker reached the memory threshold of "
+        ++ show memoryAllowance ++ " MB"
